253834bddbb4633c01a911fc0d5080784777111a
priyanka <priyanka.mukhi@3SCSOLUTION.LAN>
2022-03-16 11:40:13 +0530

    files added

---
+++ pattern star/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pattern_star
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+   
+          for (int i=1;i<=3;i++)
+          {
+            for (int j=1;j<=i;j++)
+            {
+               Console.Write("*");
+            }
+                Console.WriteLine();
+          }
+                for (int i=1;i<=5;i++)
+                {
+                   for(int j=1;j<=i;j++)
+                    {
+                        Console.Write("*");
+                    }
+                      Console.WriteLine();
+                } 
+       }
+    } 
+       
+}
